3612671c2f74ea531741fe93c8b5f9bea1fc1366
jonakrs <jonakrasniqi1411@gmail.com>
2024-10-17 15:20:40 +0200

    SignUp and home

---
+++ src/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Hello User, Welcome to the home page</h1>
+</template>
+
+<script>
+export default {
+    name: 'HomePage' 
+}
+</script>
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,9 +1,72 @@
 <template>
+    <img class="logo" src="../assets/restaurant-logo.png" />
     <h1>Sign Up</h1>
+    <div class="register">
+        <input type="text" v-model="name" placeholder="Enter Name" />
+        <input type="text" v-model="email" placeholder="Enter Email" />
+        <input type="password" v-model="password" placeholder="Enter Password" />
+        <button v-on:click="signUp">Sign Up</button>
+
+
+    </div>
+
+
 </template>
 
 <script>
-export default{
-    name:'SignUp'
+import axios from 'axios';
+export default {
+    name: 'SignUp',
+    data() {
+        return {
+            name: '',
+            email: '',
+            password: ''
+        };
+    },
+    methods: {
+        async signUp() {
+            let result = await axios.post("http://localhost:3000/user", {
+                name: this.name,
+                email: this.email,
+                password: this.password
+            });
+            console.warn(result);
+
+            if (result.status === 201) {
+                localStorage.setItem("user-info", JSON.stringify(result.data))
+                this.$router.push({name:'Home'})
+            }
+        }
+    }
 }
 </script>
+
+
+
+
+<style>
+.logo {
+    width: 100px;
+}
+
+.register input {
+    width: 300px;
+    height: 40px;
+    padding-left: 20px;
+    display: block;
+    margin-bottom: 30px;
+    margin-right: auto;
+    margin-left: auto;
+    border: 1px solid skyblue;
+}
+
+.register button {
+    width: 320px;
+    height: 40px;
+    border: 1px solid salmon;
+    background: salmon;
+    color: #fff;
+    cursor: pointer;
+}
+</style>
